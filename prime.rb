781f71d8469b927ab9ae77ddfe1fa45fcb3d1255
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-06 22:46:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
-def prime(n)
-  unless n.is_a? 
-  puts "That's not an integer."
-  if n%2 !=0 && n!=2
-    puts "#{n} is prime!"
-  else
-    puts "#{n} is not prime."
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
   end
 end
